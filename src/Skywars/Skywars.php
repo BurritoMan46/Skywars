b66b2c8d141b33ddbfd132dcfb60a49d498a131e
BurritoMan46 <32967462+BurritoMan46@users.noreply.github.com>
2018-02-07 09:33:38 +0000

    Update Skywars.php

--- src/Skywars/Skywars.php
+++ src/Skywars/Skywars.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace Skywars;
+
+use pocketmine\level\Level;
+use pocketmine\math\AxisAlignedBB;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Chest;
+use pocketmine\tile\Tile;
+use Skywars\db\lifeboat\LifeboatDatabase;
+use Skywars\gadget\GadgetManager;
+use Skywars\game\GameManager;
+use Skywars\game\GameMapSourceInfo;
+use Skywars\game\JoinGameTask;
+use Skywars\game\kit\KitManager;
+use Skywars\particle\ParticleManager;
+use Skywars\player\PlayerManager;
+use Skywars\npc\NPCManager;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\math\Vector3;
+use pocketmine\utils\Config;
+use Kits\Kit;
+use LbCore\language\Translate;
